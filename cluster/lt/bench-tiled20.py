19b3d0491ff1ddf5a7218be9cb30a25d8cbb218b
Alexander Clausen <alex@gc-web.de>
2019-03-04 11:18:58 +0100

    bench-tiled20: more parameters
    I/O methods, scan size, allow local dask cluster and control number of
    workers, number of masks

--- cluster/lt/bench-tiled20.py
+++ cluster/lt/bench-tiled20.py
@@ -12,6 +12,7 @@ from libertem.executor.dask import DaskJobExecutor
 from libertem.io.dataset.raw_direct import DirectRawFileDataSet
 from libertem.io.dataset.raw import RawFileDataSet
 import numpy as np
+import psutil
 
 
 def _preload():
@@ -20,11 +21,21 @@ def _preload():
 
 @click.command()
 @click.option('--path', default="/data/tiled20.raw")
-@click.option('--scheduler-uri')
+@click.option('--scheduler-uri', default=None)
 @click.option('--stackheight', default=32)
-@click.option('--direct/--no-direct', default=True)
-def main(path, scheduler_uri, stackheight, direct):
-    dask_executor = DaskJobExecutor.connect(scheduler_uri)
+@click.option('--scan-size', default="10240,768")
+@click.option('--method', type=click.Choice(['direct', 'mmap', 'read']))
+@click.option('--num-masks', default=1)
+@click.option('--num-workers', default=psutil.cpu_count(logical=False))
+def main(path, scheduler_uri, stackheight, scan_size, method, num_masks, num_workers):
+    scan_size = tuple(int(x) for x in scan_size.split(","))
+    if scheduler_uri is None:
+        dask_executor = DaskJobExecutor.make_local(cluster_kwargs={
+            'threads_per_worker': 1,
+            'n_workers': num_workers,
+        })
+    else:
+        dask_executor = DaskJobExecutor.connect(scheduler_uri)
     ctx = Context(executor=dask_executor)
 
     workers = ctx.executor.get_available_workers()
@@ -32,19 +43,28 @@ def main(path, scheduler_uri, stackheight, direct):
         ctx.executor.client.run(_preload, workers=[worker['name']])
 
     def _load():
-        if direct:
+        if method == "direct":
             ds = DirectRawFileDataSet(
                 path=path,
                 dtype="float32",
-                scan_size=(20 * 512, 768),
+                scan_size=scan_size,
                 detector_size=(128, 128),
                 stackheight=stackheight,
             )
-        else:
+        elif method == "read":
+            ds = DirectRawFileDataSet(
+                path=path,
+                dtype="float32",
+                scan_size=scan_size,
+                detector_size=(128, 128),
+                stackheight=stackheight,
+                enable_direct=False,
+            )
+        elif method == "mmap":
             ds = RawFileDataSet(
                 path=path,
                 dtype="float32",
-                scan_size=(20 * 512, 768),
+                scan_size=scan_size,
                 detector_size_raw=(128, 128),
                 crop_detector_to=(128, 128),
             )
@@ -58,14 +78,20 @@ def main(path, scheduler_uri, stackheight, direct):
     ds = dask_executor.run_function(_load)
     dask_executor.run_function(ds.check_valid)
 
-    apply_ring = ctx.create_ring_analysis(dataset=ds)
+    def _make_random_mask():
+        return np.random.randn(128, 128).astype("float32")
+
+    apply_mask = ctx.create_mask_analysis(
+        dataset=ds,
+        factories=num_masks * [_make_random_mask]
+    )
 
     # dry-run:
-    ctx.run(apply_ring)
+    ctx.run(apply_mask)
 
     # timed run:
     t0 = time.time()
-    ctx.run(apply_ring)
+    ctx.run(apply_mask)
     t1 = time.time()
     delta = t1 - t0
 
