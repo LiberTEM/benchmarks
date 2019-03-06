a76dba6c431a9a85f601aec213e4f47dd90a4bfe
Alexander Clausen <alex@gc-web.de>
2019-03-06 15:23:16 +0100

    output tilesize in get_lt_results, add README

--- cluster/get_lt_results.sh
+++ cluster/get_lt_results.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "throughput_mib,method,num_nodes,num_workers,tilesize_bytes"
 for i in "$@"; do
-    jq -r '[.throughput_mib,.method,.num_nodes,(.workers|length)]|@csv' < $i;
+    jq -r '[.throughput_mib,.method,.num_nodes,(.workers|length),.tilesize_bytes]|@csv' < $i;
 done
