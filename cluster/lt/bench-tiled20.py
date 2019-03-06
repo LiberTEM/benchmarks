faa66459c466d70b666c62d49d7a279c018c6d63
Alexander Clausen <alex@gc-web.de>
2019-03-06 12:39:31 +0100

    update bench script and orchestration
     + allow to limit to number of workers
     + output results as json

--- cluster/lt/bench-tiled20.py
+++ cluster/lt/bench-tiled20.py
@@ -13,6 +13,29 @@ from libertem.io.dataset.raw_direct import DirectRawFileDataSet
 from libertem.io.dataset.raw import RawFileDataSet
 import numpy as np
 import psutil
+import json
+
+
+class BenchmarkDaskExecutor(DaskJobExecutor):
+    def __init__(self, node_limit=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._node_limit = node_limit
+
+    def get_available_workers(self):
+        """
+        returns list of dict with keys 'name' and 'host'
+        """
+        workers = super().get_available_workers()
+        if self._node_limit is None:
+            return workers
+        hosts = list(sorted(set([w['host'] for w in workers])))
+        hosts_to_use = {*hosts[:self._node_limit]}
+
+        return [
+            w
+            for w in workers
+            if w['host'] in hosts_to_use
+        ]
 
 
 def _preload():
@@ -27,15 +50,18 @@ def _preload():
 @click.option('--method', type=click.Choice(['direct', 'mmap', 'read']))
 @click.option('--num-masks', default=1)
 @click.option('--num-workers', default=psutil.cpu_count(logical=False))
-def main(path, scheduler_uri, stackheight, scan_size, method, num_masks, num_workers):
+@click.option('--num-nodes', default=None, type=int)
+def main(path, scheduler_uri, stackheight, scan_size, method, num_masks, num_workers, num_nodes):
     scan_size = tuple(int(x) for x in scan_size.split(","))
+    if num_nodes is not None and scheduler_uri is None:
+        raise Exception("num_nodes limit only works for non-local cluster")
     if scheduler_uri is None:
-        dask_executor = DaskJobExecutor.make_local(cluster_kwargs={
+        dask_executor = BenchmarkDaskExecutor.make_local(cluster_kwargs={
             'threads_per_worker': 1,
             'n_workers': num_workers,
         })
     else:
-        dask_executor = DaskJobExecutor.connect(scheduler_uri)
+        dask_executor = BenchmarkDaskExecutor.connect(scheduler_uri, node_limit=num_nodes)
     ctx = Context(executor=dask_executor)
 
     workers = ctx.executor.get_available_workers()
@@ -74,9 +100,11 @@ def main(path, scheduler_uri, stackheight, scan_size, method, num_masks, num_wor
     def _getsize():
         return os.stat(path).st_size
 
-    total_size = dask_executor.run_function(_getsize)
     ds = dask_executor.run_function(_load)
     dask_executor.run_function(ds.check_valid)
+
+    total_size = dask_executor.run_function(_getsize)
+    assert total_size == np.dtype(ds.dtype).itemsize * ds.shape.size
 
     def _make_random_mask():
         return np.random.randn(128, 128).astype("float32")
@@ -95,8 +123,20 @@ def main(path, scheduler_uri, stackheight, scan_size, method, num_masks, num_wor
     t1 = time.time()
     delta = t1 - t0
 
-    print("{} bytes in {}s = {}MiB/s".format(total_size, delta, total_size / delta / 1024 / 1024))
-    print("tilesize_bytes = {}KiB".format(stackheight * 128 * 128 * 4 / 1024))
+    tilesize_bytes = stackheight * 128 * 128 * 4
+
+    results = {
+        "path": path,
+        "num_masks": num_masks,
+        "bytes": total_size,
+        "time": delta,
+        "throughput_mib": total_size / delta / 1024 / 1024,
+        "tilesize_bytes": tilesize_bytes,
+        "method": method,
+        "num_nodes": num_nodes,
+        "workers": workers,
+    }
+    print(json.dumps(results, indent=4))
 
 
 if __name__ == "__main__":
