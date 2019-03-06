4dbb0ebf410185ab2e6d29a11fd9c386a3a1ed2f
Alexander Clausen <alex@gc-web.de>
2019-03-07 00:09:36 +0100

    cpu-bound multi-node WIP

--- cluster/get_lt_results.sh
+++ cluster/get_lt_results.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "throughput_mib,method,num_nodes,num_workers,tilesize_bytes"
+echo "throughput_mib,method,num_nodes,num_workers,tilesize_bytes,path"
 for i in "$@"; do
-    jq -r '[.throughput_mib,.method,.num_nodes,(.workers|length),.tilesize_bytes]|@csv' < $i;
+    jq -r '[.throughput_mib,.method,.num_nodes,(.workers|length),.tilesize_bytes,.path]|@csv' < $i;
 done
