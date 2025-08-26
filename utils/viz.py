31d290df4e0a4c9f35a09d669965cd4efe5849c6
Alexander Clausen <a.clausen@fz-juelich.de>
2025-08-26 09:36:33 +0200

    Compare dependency versions and output a summary

--- utils/viz.py
+++ utils/viz.py
@@ -1,3 +1,7 @@
+# deps: panel, bokeh, pygments, pandas
+
+import datetime
+
 import requests
 import panel as pn
 from panel.io import hold
@@ -24,22 +28,60 @@ def to_dataframe(raw_json_object, run: str):
     return pd.DataFrame(rows, columns=header)
 
 
-dataframes = []
-filenames = []
-for i in range(6):
-    fn = f"{i + 1:05d}.json"
-    r = requests.get(
-        f"https://raw.githubusercontent.com/LiberTEM/benchmarks/refs/"
-        f"heads/main/collected/LiberTEM/LiberTEM/cpu/{fn}"
-    )
-    df_this = to_dataframe(r.json(), fn)
-    filenames.append(fn)
-    dataframes.append(df_this)
+def _version_set(version_list):
+    return set(tuple(x) for x in version_list)
 
-df = pd.concat(dataframes, ignore_index=True)
 
-# use this as y axis to "dodge" individual runs:
-df["y_factor"] = list(zip(df['name'], df['run_short']))
+def get_version_info(raw_data):
+    raw_data_sorted = list(sorted(raw_data, key=lambda bs: datetime.datetime.fromisoformat(bs[0]["datetime"])))
+    previous = None
+    previous_fn = None
+    changes = {}
+    for bs, filename in raw_data_sorted:
+        if previous is None:
+            # this is the first benchmark run we are looking at, so nothing to
+            # compare here:
+            previous = bs
+            previous_fn = filename
+            continue
+        this_set = _version_set(bs['machine_info']['freeze'])
+        prev_set = _version_set(previous['machine_info']['freeze'])
+        if this_set != prev_set:
+            # something changed, note down what:
+            changes[(previous_fn, filename)] = {
+                'minus': list(sorted(prev_set - this_set, key=lambda v: v[0])),
+                'plus': list(sorted(this_set - prev_set, key=lambda v: v[0])),
+            }
+        previous = bs
+        previous_fn = filename
+    return changes
+
+
+def load_data():
+    dataframes = []
+    filenames = []
+    raw_data = []
+    for i in range(6):
+        fn = f"{i + 1:05d}.json"
+        r = requests.get(
+            f"https://raw.githubusercontent.com/LiberTEM/benchmarks/refs/"
+            f"heads/main/collected/LiberTEM/LiberTEM/cpu/{fn}"
+        )
+        raw_json = r.json()
+        raw_data.append((raw_json, fn))
+        df_this = to_dataframe(raw_json, fn)
+        filenames.append(fn)
+        dataframes.append(df_this)
+
+    df = pd.concat(dataframes, ignore_index=True)
+    # use this as y axis to "dodge" individual runs:
+    df["y_factor"] = list(zip(df['name'], df['run_short']))
+
+    version_info = get_version_info(raw_data)
+
+    return df, version_info, filenames
+
+df, version_info, filenames = load_data()
 
 selectable = df["bench_group"].unique().tolist()
 
@@ -99,7 +141,22 @@ def update_results(e):
 
 select_test.param.watch(update_results, "value")
 
+
+def _format_version_info(version_info):
+    parts = ["# Version changes summary"]
+    for (old_fn, new_fn), changes in version_info.items():
+        parts.append(f"## {old_fn} → {new_fn}")
+        old = ["```diff"] + [f"- {name}@({version})" for (name, version) in changes['minus']] + []
+        new = [] + [f"+ {name}@({version})" for (name, version) in changes['plus']] + ["```"]
+        parts.append("\n".join(old))
+        parts.append("\n".join(new))
+    return "\n".join(parts)
+
+
+version_info_md = pn.pane.Markdown(object=_format_version_info(version_info))
+
 pn.Column(
     select_test,
     bokeh_plot,
+    version_info_md,
 ).servable()
